85e5b81368460414c32d9c4ba89666eb7a9b484d
kosarchuksn <sergeykosarchuk@gmail.com>
2020-04-04 17:51:02 +0300

    Install cors

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ import passport from 'passport';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cors from 'cors';
 
 import authRouter from './auth/router.js';
 import burgerBuilderRouter from './burger-builder/router.js';
 
 const app = express();
 app.use(helmet());
+app.use(cors())
 app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(authRouter);
